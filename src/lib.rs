bf4e47a2a20778b50843f83a9c1647bd71716bc4
Danil Karpenko <limpix31@gmail.com>
2024-05-08 20:09:05 +0200

    feat: async service

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+#[cfg(feature = "async-service")]
+pub mod async_service;
 mod bindings;
 pub mod config;
 pub mod engine;
 pub mod error;
 pub mod yuv;
 
+#[cfg(feature = "async-service")]
+pub use async_service::*;
 pub use config::Config;
 pub use engine::*;
 pub use error::*;
